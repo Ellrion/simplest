ca9a5037c78566aaad0d36b72e2c4539352d3e9b
Maksim (Ellrion) Platonov <ellrion11@gmail.com>
2017-01-25 14:47:44 +0300

    upd example template

--- views/welcome.php
+++ views/welcome.php
@@ -1,14 +1,23 @@
-<!doctype html>
-<html lang="en-US">
+<!DOCTYPE html>
+<html lang="ru-Ru">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= e($name) ?></title>
+
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css">-->
+    <!--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/journal/bootstrap.min.css">-->
     <link rel="stylesheet" type="text/css" href="/css/main.css">
 </head>
 <body>
+<div class="container">
     <h1><?= e($name) ?></h1>
     <h2>Welcome</h2>
-
-    <script src="/js/app.js"></script>
+</div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="/js/app.js"></script>
 </body>
 </html>
